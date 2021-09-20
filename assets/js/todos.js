129a71fa5dae63f8fc37dc7be5b2a0190c552198
Shivam Raj Singh <48837530+shivam3009@users.noreply.github.com>
2021-09-20 21:56:15 +0530

    js changed

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,20 +1,26 @@
-// alert("connected");
-$("ul").on("click" , "li" ,function(){
+// Check Off Specific Todos By Clicking
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
 
-$("ul").on("click" , "span" ,function(event){
-	$(this).parent().fadeOut(500 ,function (){ 
+//Click on X to delete Todo
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500,function(){
 		$(this).remove();
 	});
-	event.stopPropagation(); // will stop propagation of effect to its parents
+	event.stopPropagation();
 });
 
-$("input[type = 'text']").on("keypress" , function(event){
+$("input[type='text']").keypress(function(event){
 	if(event.which === 13){
-		var todoText = $(this).val(); // extracts the value
-		$(this).val(""); // for clearing the input
-		$("ul").append("<li><span></span>" + todoText + "</li>"); // append helps in adding in values on run time which takes input html and add it
-
+		//grabbing new todo text from input
+		var todoText = $(this).val();
+		$(this).val("");
+		//create a new li and add to ul
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
 	}
+});
+
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
 });
